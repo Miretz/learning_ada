fb6f0c58f9f79a4054557829ecc57a9efc3adf97
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-22 17:52:41 +0100

    records renaming

--- Records/src/main.adb
+++ Records/src/main.adb
@@ -1,7 +1,16 @@
+with Ada.Text_IO; use Ada.Text_IO;
 with Record_Selection;
+with Dates; use Dates;
 
 procedure Main is
-
+   D : Date := (1, January, 2000);
 begin
    Record_Selection;
+
+   Put_Line ("Example using renaming: ");
+   Display_Month(D);
+   Put_Line ("Increasing month...");
+   Increase_Month(D);
+   Display_Month(D);
+
 end Main;
