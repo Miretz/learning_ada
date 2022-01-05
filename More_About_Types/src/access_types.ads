19491f8696926f7f0011cad7003cb37602008075
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-05 11:03:17 +0100

    Allocation by type

--- More_About_Types/src/access_types.ads
+++ More_About_Types/src/access_types.ads
@@ -4,7 +4,7 @@ with Dates; use Dates;
 package Access_Types is
 
    type Date_Acc is access Date;
-   type Date_Acc_2 is access Date;
+   type Date_Acc_2 is access all Date;
 
    D : Date_Acc := null; -- null pointer
 
@@ -14,4 +14,16 @@ package Access_Types is
    -- need to do type conversion
    D3 : Date_Acc_2 := Date_Acc_2(D);
 
-end Access_Types
+   -- allocation
+   DD : Date_Acc := new Date;
+
+   -- size allocation
+   type String_Acc is access String;
+   Msg : String_Acc; -- null by default
+   -- constraint is needed when allocating String
+   Buffer : String_Acc := new String(1 .. 10);
+
+   DDD : Date_Acc := new Date'(5, January, 2022);
+   MsgMsg : String_Acc := new String'("Hello");
+
+end Access_Types;
