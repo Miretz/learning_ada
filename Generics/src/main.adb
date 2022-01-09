278538042b1f572d743c7450a7c8d45976cc7069
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-09 09:57:41 +0100

    Generic packages

--- Generics/src/main.adb
+++ Generics/src/main.adb
@@ -1,8 +1,18 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
 with Operator;
 with Set;
 with Show_Generic_Instantiation;
+with Show_Generic_Package;
 
 procedure Main is
 begin
+   Put_Line ("Show_Generic_Instantiation");
    Show_Generic_Instantiation;
+   New_Line;
+
+   Put_Line ("Show_Generic_Package");
+   Show_Generic_Package;
+   New_Line;
+
 end Main;
