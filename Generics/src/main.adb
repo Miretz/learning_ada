053f5520759cc55acc16ce938d5012e9f9911c78
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-09 10:21:33 +0100

    Example IO instances

--- Generics/src/main.adb
+++ Generics/src/main.adb
@@ -6,6 +6,10 @@ with Show_Generic_Instantiation;
 with Show_Generic_Package;
 with Show_Formal_Subprogram;
 
+with Show_Float_Text_IO;
+with Show_Float_IO_Inst;
+with Show_Decimal_IO_Inst;
+
 procedure Main is
 begin
    Put_Line ("Show_Generic_Instantiation");
@@ -19,5 +23,11 @@ begin
    Put_Line ("Show_Formal_Subprogram");
    Show_Formal_Subprogram;
    New_Line;
+
+   Put_Line ("Example IO Instances");
+   Show_Float_Text_IO;
+   New_Line;
+   Show_Float_IO_Inst;
+   Show_Decimal_IO_Inst;
 
 end Main;
