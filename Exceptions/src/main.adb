db89b2e42402e35439ebf74c7fe606b9ec2f64a3
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-12 17:56:40 +0100

    Exceptions: Predefined exceptions

--- Exceptions/src/main.adb
+++ Exceptions/src/main.adb
@@ -3,7 +3,7 @@ with Open_File;
 with Be_Careful;
 
 procedure Main is
-
+   Ada_Learning_Exception : exception;
 begin
 
    -- raise My_Except;
@@ -11,6 +11,24 @@ begin
 
    -- Open_File;
 
-   Be_Careful;
+   -- Be_Careful;
+
+   -- Ada has a very small number of predefined exceptions
+
+   -- raise Constraint_Error;
+   -- Used for bounds violation, overflow, null derefrence, division by 0
+
+   -- raise Program_Error;
+   -- raised in more arcane situations, execution errors, etc.
+
+   -- raise Storage_Error;
+   -- memory issues, not enough memory, not enough stack
+
+   -- raise Tasking_Error;
+   -- task related errors
+
+   -- You should always define your own exception objects
+
+   raise Ada_Learning_Exception with "I'm still learning";
 
 end Main;
