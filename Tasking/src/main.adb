8b6d412e2bca237651caeb43f65d63cd0c7b6f55
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-12 18:12:50 +0100

    Tasking: Delay

--- Tasking/src/main.adb
+++ Tasking/src/main.adb
@@ -1,11 +1,13 @@
-with Simple_Task;
-with Show_Simple_Sync;
-with Test_Simple_Sync_Pkg;
+-- with Simple_Task;
+-- with Show_Simple_Sync;
+-- with Test_Simple_Sync_Pkg;
+with Show_Delay;
 
 procedure Main is
 
 begin
-   Simple_Task;
-   Show_Simple_Sync;
-   Test_Simple_Sync_Pkg;
+   -- Simple_Task;
+   -- Show_Simple_Sync;
+   -- Test_Simple_Sync_Pkg;
+   Show_Delay;
 end Main;
