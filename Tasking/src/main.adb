27e22857e8fbb25755813af2202911b234a48295
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-13 21:02:58 +0100

    Tasking: Select loop

--- Tasking/src/main.adb
+++ Tasking/src/main.adb
@@ -2,7 +2,8 @@
 -- with Show_Simple_Sync;
 -- with Test_Simple_Sync_Pkg;
 -- with Show_Delay;
-with Show_Rendezvous;
+-- with Show_Rendezvous;
+with Show_Rendezvous_Loop;
 
 procedure Main is
 
@@ -11,5 +12,6 @@ begin
    -- Show_Simple_Sync;
    -- Test_Simple_Sync_Pkg;
    -- Show_Delay;
-   Show_Rendezvous;
+   -- Show_Rendezvous;
+   Show_Rendezvous_Loop;
 end Main;
