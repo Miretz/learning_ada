36ebe5acfda109407a6e1b5ad3e97afc0a39f5ff
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-19 18:52:08 +0100

    Simple contract

--- Design_By_Contracts/src/main.adb
+++ Design_By_Contracts/src/main.adb
@@ -1,9 +1,11 @@
 with Show_Simple_Precondition;
 with Show_Simple_Postcondition;
+with Show_Simple_Contract;
 
 procedure Main is
 
 begin
    Show_Simple_Precondition;
    Show_Simple_Postcondition;
+   Show_Simple_Contract;
 end Main;
