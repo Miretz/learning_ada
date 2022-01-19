a2e0501b8f271759c1c4b54097c94fdf590c6bce
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-19 19:05:28 +0100

    Dynamic predicate example

--- Design_By_Contracts/src/main.adb
+++ Design_By_Contracts/src/main.adb
@@ -1,11 +1,13 @@
-with Show_Simple_Precondition;
-with Show_Simple_Postcondition;
-with Show_Simple_Contract;
+-- with Show_Simple_Precondition;
+-- with Show_Simple_Postcondition;
+-- with Show_Simple_Contract;
+with Show_Dynamic_Predicate_Courses;
 
 procedure Main is
 
 begin
-   Show_Simple_Precondition;
-   Show_Simple_Postcondition;
-   Show_Simple_Contract;
+   -- Show_Simple_Precondition;
+   -- Show_Simple_Postcondition;
+   -- Show_Simple_Contract;
+   Show_Dynamic_Predicate_Courses;
 end Main;
