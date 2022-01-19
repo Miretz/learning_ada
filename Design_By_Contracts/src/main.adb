a5b63fc823866f4c168ab109c0ad200f94d20e52
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-19 18:35:48 +0100

    Design by Contracts: Simple precondition

---
+++ Design_By_Contracts/src/main.adb
@@ -0,0 +1,7 @@
+with Show_Simple_Precondition;
+
+procedure Main is
+
+begin
+   Show_Simple_Precondition;
+end Main;
---
+++ Design_By_Contracts/src/show_simple_precondition.adb
@@ -0,0 +1,18 @@
+-- don't forget to add -gnata switch to the project to enable assertions
+
+procedure Show_Simple_Precondition is
+
+   procedure DB_Entry (Name : String; Age : Natural) with
+      Pre => Name'Length > 0
+   is
+   begin
+      -- Missing implementation
+      null;
+   end DB_Entry;
+
+begin
+   DB_Entry ("John", 30);
+
+   -- Precodnition will fail because the name length is 0
+   DB_Entry ("", 21);
+end Show_Simple_Precondition;
