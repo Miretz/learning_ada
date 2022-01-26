c088e62a3e4a0ffe12999c570bdd377dac3c2e1f
miretz <1217795+Miretz@users.noreply.github.com>
2022-01-26 01:01:21 +0100

    interfacing with C

---
+++ Interfacing_With_C/src/main.adb
@@ -0,0 +1,7 @@
+with Show_C_Enum;
+
+procedure Main is
+
+begin
+   Show_C_Enum;
+end Main;
---
+++ Interfacing_With_C/src/show_c_enum.adb
@@ -0,0 +1,8 @@
+procedure Show_C_Enum is
+
+   type C_Enum is (A, B, C)
+     with Convention => C;
+
+begin
+   null;
+end Show_C_Enum;
