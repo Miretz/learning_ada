981afd47e6b1a77248739f425ab086c2feb98063
miretz <1217795+Miretz@users.noreply.github.com>
2022-02-10 11:01:36 +0100

    Classwide types

--- Object_Oriented_Programming/src/main.adb
+++ Object_Oriented_Programming/src/main.adb
@@ -1,7 +1,17 @@
 with Primitives;
-with P;
+with P; use P;
 
 procedure Main is
+   Obj_1 : My_Class;
+   Obj_2 : Derived := (A => 12);
+
+   -- Obj_3 : My_Class := 02; -- invalid, error assigning derived type
+
+   -- to fix the above
+   Obj_3 : My_Class'Class := Obj_2;
+   -- My_Class'Class designates classwide type for My_Class
+   -- which includes all descending types
+
 begin
    Primitives;
 end Main;
